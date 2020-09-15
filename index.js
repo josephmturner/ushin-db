af6c894a68b0424970631ea194e3bb8045aca85f
mauve <ranger@mauve.moe>
2020-09-15 12:33:01 -0400

    Initial version with tests

---
+++ index.js
@@ -0,0 +1,150 @@
+const PouchDB = require("pouchdb");
+PouchDB.plugin(require("pouchdb-find"));
+
+const AUTHOR_KEY = "author";
+
+// Based on USHIN data model
+// https://github.com/USHIN-Inc/ushin-app/blob/master/src/dataModels.ts
+
+class USHINBase {
+  constructor({ leveldown, authorURL }) {
+    this.leveldown = leveldown;
+    this.db = new PouchDB("ushin-db", {
+      adapter: "leveldb",
+      db: leveldown,
+    });
+    this.authorURL = authorURL;
+    this.loadDBForAuthor;
+  }
+
+  async init() {
+    // TODO create indexes here based on the sorts of queries we want
+    await this.db.createIndex({
+      index: {
+        fields: ["type"],
+      },
+    });
+    await this.db.createIndex({
+      index: {
+        fields: ["type", "createdAt"],
+      },
+    });
+  }
+
+  async setAuthorInfo(info = {}) {
+    const { _rev, _id, ...data } = this.getAuthorInfo();
+    await this.db.put({
+      ...data,
+      ...info,
+      _id: AUTHOR_KEY,
+      _rev,
+    });
+  }
+
+  async getAuthorInfo() {
+    try {
+      const info = await this.db.get(AUTHOR_KEY);
+      return info;
+    } catch (e) {
+      return { _id: AUTHOR_KEY };
+    }
+  }
+
+  async addMessage({
+    revisionOf,
+    focus,
+    main,
+    createdAt = new Date(),
+    points = {},
+  }) {
+    const { authorURL } = this;
+    const finalPoints = {};
+    const createdAtTime = createdAt.getTime();
+
+    for (const shape in points) {
+      const originalPoints = points[shape];
+      const pointPromises = originalPoints.map((point) =>
+        this.addPoint({ createdAt: createdAtTime, ...point })
+      );
+      const pointIDs = await Promise.all(pointPromises);
+      finalPoints[shape] = pointIDs;
+    }
+
+    const { id } = await this.db.post({
+      type: "message",
+      revisionOf,
+      focus,
+      main,
+      createdAt: createdAtTime,
+      author: authorURL,
+      points: finalPoints,
+    });
+
+    return id;
+  }
+
+  async getMessage(id) {
+    const rawMessage = await this.db.get(id);
+    const { points: rawPoints, createdAt } = rawMessage;
+
+    const finalPoints = {};
+
+    for (let shape in rawPoints) {
+      const pointIDs = rawPoints[shape];
+      const pointPromises = pointIDs.map((id) => this.getPoint(id));
+      const pointObjects = await Promise.all(pointPromises);
+      finalPoints[shape] = pointObjects;
+    }
+
+    const createdAtDate = new Date(createdAt);
+
+    return {
+      ...rawMessage,
+      points: finalPoints,
+      messageId: id,
+      createdAt: createdAtDate,
+    };
+  }
+
+  async searchMessages(selector = {}, { limit = 32, skip, sort } = {}) {
+    const finalSelector = { ...selector, type: "message" };
+    const { docs } = await this.db.find({
+      selector,
+      limit,
+      skip,
+    });
+
+    return docs;
+  }
+
+  async addPoint({
+    author,
+    content,
+    shape,
+    pointDate,
+    quotedAuthor,
+    createdAt,
+  }) {
+    const { id } = await this.db.post({
+      type: "point",
+      author,
+      content,
+      shape,
+      pointDate,
+      quotedAuthor,
+    });
+    return id;
+  }
+
+  async getPoint(id) {
+    return this.db.get(id);
+  }
+
+  async close() {
+    return this.db.close();
+  }
+}
+
+module.exports = {
+  USHINBase,
+};
---
+++ test.js
@@ -0,0 +1,87 @@
+const { USHINBase } = require("./");
+const memdown = require("memdown");
+const test = require("tape");
+
+async function getNew(authorURL = "hyper://example") {
+  const leveldown = (name) => memdown();
+  const db = new USHINBase({ leveldown, authorURL });
+
+  await db.init();
+
+  return db;
+}
+
+const EXAMPLE_MESSAGE = {
+  points: {
+    feelings: [
+      {
+        content: "Cats bring me joy",
+      },
+    ],
+  },
+};
+
+test("Able to initialize and set author metadata", async (t) => {
+  t.plan(3);
+  try {
+    var db = await getNew();
+
+    t.pass("Able to create the DB");
+
+    await db.setAuthorInfo({ name: "Example" });
+
+    t.pass("Able to set author info");
+
+    const { name } = await db.getAuthorInfo();
+
+    t.equal(name, "Example", "name got set and can be retrieved");
+  } catch (e) {
+    t.error(e);
+  } finally {
+    if (db) db.close();
+  }
+});
+
+test("Able to add and get messages", async (t) => {
+  t.plan(4);
+  try {
+    var db = await getNew("test");
+
+    const id = await db.addMessage(EXAMPLE_MESSAGE);
+
+    t.pass("Able to add message");
+
+    const message = await db.getMessage(id);
+
+    const { author, points, createdAt } = message;
+    const { feelings } = points;
+    const [point] = feelings;
+    t.equal(author, "test", "Author got set");
+    t.equal(feelings.length, 1, "Feelings got set");
+    t.equal(point.content, "Cats bring me joy", "Point content got set");
+  } catch (e) {
+    t.error(e);
+  } finally {
+    if (db) db.close();
+  }
+});
+test("Able to search for messages in a time range", async (t) => {
+  t.plan(2);
+  try {
+    var db = await getNew("test");
+
+    await db.addMessage({ createdAt: new Date(10), ...EXAMPLE_MESSAGE });
+    await db.addMessage({ createdAt: new Date(2000), ...EXAMPLE_MESSAGE });
+    await db.addMessage({ createdAt: new Date(3000), ...EXAMPLE_MESSAGE });
+
+    t.pass("Able to add several messages");
+
+    const results = await db.searchMessages({ createdAt: { $gt: 100 } });
+
+    t.equal(results.length, 2, "Got expected results");
+  } catch (e) {
+    t.error(e);
+  } finally {
+    if (db) db.close();
+  }
+});
